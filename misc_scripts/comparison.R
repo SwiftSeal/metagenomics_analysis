af97f3952afee362509a101138b7d390d73e0ca6
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-08-29 13:42:08 +0100

    update post analysis

--- misc_scripts/comparison.R
+++ misc_scripts/comparison.R
@@ -1,7 +1,7 @@
 library(phyloseq)
 library(tidyverse)
 
-emu <- readRDS("emu-phyloseq.rds")
+emu <- readRDS("results/emu-phyloseq.rds")
 dada2 <- readRDS("JH33_dada2_trimmed_silva_138.1_2.rds")
 
 # add sample data to emu
@@ -23,10 +23,13 @@ dada2 <- prune_samples(sample_names(dada2) %in% sample_names(emu), dada2)
 
 ## remove mitochondria and chloroplasts
 dada2 <- subset_taxa(dada2, (Family!="Mitochondria") | is.na(Family))
-dada2 <- subset_taxa(dada2, (Family!="Chloroplast") | is.na(Family))
+dada2 <- subset_taxa(dada2, (Order!="Chloroplast") | is.na(Order))
 
 ## remove asv with no assignment at Phylum level
 dada2 <- subset_taxa(dada2, Phylum!="")
+
+#! At this point I don't think it's worth including additional filtering steps.
+#! They went on to do some abundance filtering but for the purpose of comparing the two methods I don't think it's necessary at this stage!
 
 # make merged df of emu and dada2 richness data
 dada2_richness <- estimate_richness(dada2, measures = c("Shannon", "Simpson"))
